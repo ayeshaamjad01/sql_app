8f5d2462c2ec2cec1377fd0c4e5c0d1c0629483d
Ayesha Amjad <ashamjad007@gmail.com>
2024-09-10 12:26:50 +0500

    add to db

--- lib/data/local/db_helper.dart
+++ lib/data/local/db_helper.dart
@@ -12,17 +12,31 @@ class DBHelper {
     return DBHelper._();
   }
 
+  /// creating variables for table names and columns
+
+  /// notes table
+  static final String TABLE_NOTE = "note";
+  static final String COLUMN_NOTE_SNO = "s_no";
+  static final String COLUMN_NOTE_TITLE = "title";
+  static final String COLUMN_NOTE_DESC = "desc";
+
   // making it nullable , this means it can be null
   Database? appDB;
 
   /// db Open (path -> if exists open, else create)
   Future<Database> getDB() async {
-    if (appDB != null) {
-      return appDB!;
-    } else {
-      appDB = await openDB();
-      return appDB!;
-    }
+    /// ?? means (if null) return appDB , if its null then return openDB()
+    ///return appDB ?? await openDB();
+    ///or do it in this way
+    appDB ??= await openDB();
+    return appDB!;
+
+    // if (appDB != null) {
+    //   return appDB!;
+    // } else {
+    //   appDB = await openDB();
+    //   return appDB!;
+    // }
   }
 
   Future<Database> openDB() async {
@@ -30,10 +44,25 @@ class DBHelper {
     String dbPath = join(appDir.path, "noteDB.db");
     return await openDatabase(dbPath, onCreate: (db, version) {
       ///create all your tables here
-      ///
+      ///you can do this way also to create a table and its columns,
+      ///but it will get complex so making variables is the best choice
+      // db.execute(
+      //     "create table note (s_no integer primary key autoincrement, title text, desc text)");
+      db.execute(
+          "create table $TABLE_NOTE ($COLUMN_NOTE_SNO integer primary key autoincrement, $COLUMN_NOTE_TITLE text, $COLUMN_NOTE_DESC text)");
     }, version: 1);
   }
 
   /// all queries
-  ///
+  /// insertion query
+  Future<bool> addNote(
+      {required String note_title, required String note_desc}) async {
+    var db = await getDB();
+
+    int rowsEffected = await db.insert(TABLE_NOTE, {
+      COLUMN_NOTE_TITLE: note_title,
+      COLUMN_NOTE_DESC: note_desc,
+    });
+    return rowsEffected > 0;
+  }
 }

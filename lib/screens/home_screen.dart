ab2327f7ee39c24bd4e7c85d0280d1f427d521c8
Ayesha Amjad <ashamjad007@gmail.com>
2024-09-10 13:34:57 +0500

    static data reflected on ui

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -22,6 +22,7 @@ class _HomeScreenState extends State<HomeScreen> {
 
   void getNotes() async {
     allNotes = await dbRef!.getAllNotes();
+    setState(() {});
   }
 
   @override
@@ -30,14 +31,32 @@ class _HomeScreenState extends State<HomeScreen> {
       appBar: AppBar(
         title: Text('NOTES '),
         centerTitle: true,
-        backgroundColor: Colors.lightGreenAccent,
+        backgroundColor: Colors.orangeAccent,
       ),
       //all nites visible
-      body: ListView.builder(itemBuilder: (_, index) {}),
+      body: allNotes.isNotEmpty
+          ? ListView.builder(
+              itemCount: allNotes.length,
+              itemBuilder: (_, index) {
+                return ListTile(
+                  leading: Text('${allNotes[index][DBHelper.COLUMN_NOTE_SNO]}'),
+                  title: Text(allNotes[index][DBHelper.COLUMN_NOTE_TITLE]),
+                  subtitle: Text(allNotes[index][DBHelper.COLUMN_NOTE_DESC]),
+                );
+              },
+            )
+          : Center(child: Text('No Notes yet')),
       floatingActionButton: FloatingActionButton(
-          onPressed: () {
+          onPressed: () async {
             ///note to be added from
+            bool check = await dbRef!.addNote(
+                note_title: "First Note",
+                note_desc: "This is the firsst note description");
+            if (check) {
+              getNotes();
+            }
           },
+          backgroundColor: Colors.orangeAccent,
           child: Icon(Icons.add)),
     );
   }
